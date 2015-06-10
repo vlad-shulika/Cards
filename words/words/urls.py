2e3b91538d9ac88e6015dbd2cfcd41269d2bbb7b
Andrii Kozin <z4i.andrey@gmail.com>
2015-06-10 21:24:32 +0200

    #10 updated urls

--- words/words/urls.py
+++ words/words/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^cards/', include('cards.urls', namespace='cards'))
+    url(r'^', include('cards.urls', namespace='cards'))
 ]
