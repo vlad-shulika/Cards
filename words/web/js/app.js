1653d5f3f5b7159aa903977628674d111e8dc369
Andrii Kozin <z4i.andrey@gmail.com>
2015-06-15 08:52:27 +0200

    #7 Modified models and logic to search/add/delete

--- words/web/js/app.js
+++ words/web/js/app.js
@@ -12,22 +12,55 @@ var model = {
 var cardsApp = angular.module("cardsApp", []);
 
 cardsApp.run(function($http){
-    $http.get("stubModels/languages.json").success(function(data){
-        model.configuredLanguages = data;
-    });
-
-    $http.get("stubModels/phrases.json").success(function(data){
-        model.phrases = data;
-    });
-
-    $http.get("stubModels/cards.json").success(function(data){
-        model.cards = data;
-    });
-
     $http.get("js/rest.json").success(function(data){
         globalConfig.rest_url = data.rest_url;
         globalConfig.init = true;
+
+        // Download models
+        $http.get("stubModels/languages.json").success(function(data){
+            model.configuredLanguages = data;
+
+            // Download phrases after languages
+            $http.get("stubModels/phrases.json").success(function(data){
+                // Convert language id to Language object
+                phrasesWithLanguage = [];
+
+                data.forEach(function(phrase){
+                    modifiedPhrase = phrase;
+                    for (mLanguage of model.configuredLanguages) {
+                        if (mLanguage.id == phrase.language){
+                            modifiedPhrase.language = mLanguage;
+                            break;
+                        }
+                    }
+                    phrasesWithLanguage.push(modifiedPhrase);
+                });
+                model.phrases = phrasesWithLanguage;
+                // Download cards after phrases
+                $http.get("stubModels/cards.json").success(function(data){
+                    convertedData = [];
+                    // convert ids to model
+
+                    data.forEach(function(item){
+                        //{"id" : 1, "phrases" : [{"phrase_id": 1}, {"phrase_id" : 3}]}
+                        itemWithPhrases = {id: item.id, phrases: []};
+                        item.phrases.forEach(function(phrase){
+                            for (mPhrase of model.phrases){
+                                if (mPhrase.id == phrase.phrase_id) {
+                                    itemWithPhrases.phrases.push(mPhrase);
+                                    return;
+                                }
+                            }
+                        });
+
+                        convertedData.push(itemWithPhrases);
+                    });
+                    model.cards = convertedData;
+                });
+            });
+        });
     });
+    
 });
 
 cardsApp.filter("checkedItems", function() {
@@ -66,13 +99,21 @@ cardsApp.controller("wordsCtrl", function ($scope) {
 
         $scope.addNewPhraseItem = function(phrase, languageModel) {
             last_index = $scope.userModel.phrases.length;
-            $scope.userModel.phrases.push({id : last_index + 1, phrase : phrase, language : languageModel.id});
+            $scope.userModel.phrases.push({id : last_index + 1, phrase : phrase, language : languageModel});
         }
 
         $scope.searchForPhrase = function(card_id, phraseToSearch) {
             if (phraseToSearch) {
-                phrase = {id : 1, phrase : phraseToSearch, language_id : 1};
-                $scope.userModel.cards[card_id].searchResult = [phrase];    
+                cardToEdit = $scope.userModel.cards[card_id];
+                phrases = $scope.userModel.phrases;
+                searchResults = [];
+                for (phrase of phrases) {
+                    if (phrase.phrase.indexOf(phraseToSearch) !== -1 &&
+                        cardToEdit.phrases.indexOf(phrase) === -1) {
+                        searchResults.push(phrase);
+                    };
+                }
+                $scope.userModel.cards[card_id].searchResult = searchResults;
             }
             else {
                 $scope.userModel.cards[card_id].searchResult = [];
@@ -97,7 +138,7 @@ cardsApp.controller("wordsCtrl", function ($scope) {
             }
         }
 
-        $scope.addItemToCard = function(card_id,  phrase_item, add_to_card_flag){
+        $scope.addPhraseToCard = function(card_id,  phrase_item, add_to_card_flag){
             card = $scope.findCardById(card_id);
             if (card == null) {
                 return;
@@ -105,10 +146,19 @@ cardsApp.controller("wordsCtrl", function ($scope) {
 
             if (add_to_card_flag) {
                 card.phrases.push(phrase_item);
-            } 
-            else {
-                $scope.removePhraseFromCardById(card.phrases, phrase_item.id);
+                // remove item from search
+                $scope.removePhraseFromCardById(card.searchResult, phrase_item.id);
             }
         }
 
+        $scope.removePhraseFromCard = function(card_id,  phrase_item, remove_from_card_flag){
+            card = $scope.findCardById(card_id);
+            if (card == null) {
+                return;
+            }
+
+            if (remove_from_card_flag) {
+                $scope.removePhraseFromCardById(card.phrases, phrase_item.id);
+            }
+        }
 });
