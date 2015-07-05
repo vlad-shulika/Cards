3e76540f3b51f8fb2124d068d352469adb988998
Andrii Kozin <z4i.andrey@gmail.com>
2015-07-05 17:57:28 +0200

    removed wsgi modifications

--- words/manage.py
+++ words/manage.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-from words import configure_web
 
 if __name__ == "__main__":
-    # assume that we run our web part by MAMP on 8888 port
-    configure_web.set_rest_url('http://127.0.0.1:8000/')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "words.settings")
 
     from django.core.management import execute_from_command_line
