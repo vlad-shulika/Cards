05a56619f627b9acbba84cb50615618eaeb204e2
Andrii Kozin <z4i.andrey@gmail.com>
2015-10-13 21:35:44 +0200

    Connection class enhencments

--- words/web/tests/specs/rest-tests.js
+++ words/web/tests/specs/rest-tests.js
@@ -8,8 +8,20 @@ describe("RestTests", function() {
     });
 
     it("should be able to receive languages", function() {
-        connection.download("languages", {}, function(error, data) {
-            expect(error).toEqual(0);
+        var callback = jasmine.createSpy();
+        connection.download(Connection.OBJECT_TYPES.LANGUAGE, {}, callback);
+        waitsFor(function() {
+            return callback.callCount > 0;
+        }, "Download timed out", 5000);
+
+        runs(function () {
+            expect(callback).toHaveBeenCalled();
+        });
+    });
+
+    it("should not be able to receive object due unsupported type", function() {
+        connection.download(-1, {}, function(error, data) {
+            expect(error).toEqual(Connection.ERRORS.TYPE_IS_NOT_SUPPORTED);
         });
     });
 });
