8b244cdb7e868a10c98de1edb80020eccf786622
Andrii Kozin <z4i.andrey@gmail.com>
2015-12-03 21:53:53 +0100

    Issue #25, use swagger as ui

--- nodejs/controllers/apiController.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-    app.get('/api/language/:id', function(req, res) {
-        res.json({name: 'English', id: 1});
-    });
-}
--- nodejs/controllers/htmlController.js
+++
@@ -1,8 +0,0 @@
-var bodyParser = require('body-parser');
-var urlEncodedParser = bodyParser.urlencoded({ extended: false});
-
-module.exports = function(app) {
-    app.get('/', function(req, res) {
-        res.render('index');
-    });
-}
